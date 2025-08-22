a30dc986652c71722d8777acd13ace12ef8d9098
Austin Nowak <austinn2603@gmail.com>
2025-08-22 11:27:42 +0000

    Fix some stuff

--- news_api_email/main.py
+++ news_api_email/main.py
@@ -13,19 +13,18 @@ receiver = os.getenv("RECEIVER")
 
 topic = "tesla"
 
-api_url = f"{api_base_url}&apiKey={key}&q={topic}&from=2025-07-22&sortBy=publishedAt&language=en&pageSize=20"
+api_url = f"{api_base_url}?apiKey={key}&q={topic}&from=2025-07-22&sortBy=publishedAt&language=en&pageSize=20"
+
+print(api_url)
 
 req = requests.get(api_url, timeout=5000)
 data = req.json()
 
-body = ""
+msg = "Subject: Latest news" + "\n"
 for article in data["articles"]:
-    if article["title"] is not None:
-        body = (
-            "Subject: Today's news"
-            + "\n"
-            + body
-            + article["title"]
+    if article["title"] and article["description"]:
+        msg += (
+            article["title"]
             + "\n"
             + article["description"]
             + "\n"
@@ -33,5 +32,6 @@ for article in data["articles"]:
             + 2 * "\n"
         )
 
-body = body.encode("utf-8")
-send_email(username, password, receiver, body)
+msg = msg.encode("utf-8")
+
+send_email(username, password, receiver, msg)
